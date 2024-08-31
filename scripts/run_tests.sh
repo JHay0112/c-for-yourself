6aaeaadd3de3b543edd4e53c4180d312a65c0128
JHay0112 <jordanhay0112@gmail.com>
2024-08-31 21:55:40 +1200

    Increase rigour of test system and reporting

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,15 +4,66 @@
 #
 # Author: J. L. Hay
 
-BUILD_DIR=build
-TEST_DIR=tests
+TEST_DIR=build/tests
+TEST_LIB_DIR=lib/unity
+RESULT_DIR=results
+
 TEST_EXT=out
+RESULT_EXT=result
+PASS_EXT=testpass
+FAIL_EXT=testfail
+
+SUMMARY_FILE=${RESULT_DIR}/results.summary
+
+PASS="PASS"
+FAIL="FAIL"
+IGNORE="IGNORE"
+
+
+# Remove old results
+rm -rf ${RESULT_DIR}
+
 
 # Setup test programs
-make tests
+make tests > /dev/null ||
+    exit_on_error "Tests failed to build."
 
-# Find test programs
-tests=$(find $BUILD_DIR/$TEST_DIR/ -name "*.$TEST_EXT" -type f)
+# Setup test summary file
+mkdir -p ${RESULT_DIR}
+> ${SUMMARY_FILE}
 
+
+# Find test programs and execute
+tests=$(find ${TEST_DIR} -name "*.${TEST_EXT}" -type f)
 chmod +x $tests
-$tests
+for test in $tests 
+do
+    test_file=${test%.${TEST_EXT}}
+
+    echo > ${test_file}.${RESULT_EXT}
+    echo ${test_file} >> ${test_file}.${RESULT_EXT}
+    ${test} >> ${test_file}.${RESULT_EXT}
+
+    cat ${test_file}.${RESULT_EXT} >> ${SUMMARY_FILE}
+
+    # Convert extension to pass or fail
+    if grep -q -e ${FAIL} -e ${IGNORE} ${test_file}.${RESULT_EXT}
+    then
+        mv ${test_file}.${RESULT_EXT} ${test_file}.${FAIL_EXT}
+    else
+        mv ${test_file}.${RESULT_EXT} ${test_file}.${PASS_EXT}
+    fi
+done
+
+
+# Move results into the results directory
+results=$(find ${TEST_DIR} -name "*.${PASS_EXT}" -o -name "*.${FAIL_EXT}" -type f)
+mv ${results} ${RESULT_DIR}
+
+# Summarise the results
+ruby ${TEST_LIB_DIR}/auto/unity_test_summary.rb ${RESULT_DIR} >> ${SUMMARY_FILE}
+cat ${SUMMARY_FILE}
+
+
+# Determine exit status from number of failures
+exit $(find . -name "*.${FAIL_EXT}" | wc -l)
